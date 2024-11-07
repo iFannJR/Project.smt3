f4ab3e969b8b4c9d3db3904f11bd28ac35fd1e8b
Muhammad Fikri Arifandi <158024864+iFannJR@users.noreply.github.com>
2024-11-07 20:46:13 +0700

    animasi js

---
+++ main.js
@@ -0,0 +1,12 @@
+// Animations
+const registerButton = document.getElementById("register");
+const loginButton = document.getElementById("login");
+const container = document.getElementById("container");
+
+registerButton.addEventListener("click", () => {
+  container.classList.add("right-panel-active");
+});
+
+loginButton.addEventListener("click", () => {
+  container.classList.remove("right-panel-active");
+});
