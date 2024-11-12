a3cbe143fad8840b759af021f1682f5a9b04a624
Muhammad Fikri Arifandi <158024864+iFannJR@users.noreply.github.com>
2024-11-12 21:32:40 +0700

    perbaikan login fix
    tambah foto dan edit warna

--- main.js
+++ main.js
@@ -1,12 +1,8 @@
 // Animations
-const registerButton = document.getElementById("register");
-const loginButton = document.getElementById("login");
-const container = document.getElementById("container");
+// const loginButton = document.getElementById("login");
+// const container = document.getElementById("container");
 
-registerButton.addEventListener("click", () => {
-  container.classList.add("right-panel-active");
-});
 
-loginButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
-});
+// loginButton.addEventListener("click", () => {
+//   container.classList.remove("right-panel-active");
+// });
